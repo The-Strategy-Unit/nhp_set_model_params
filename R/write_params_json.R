f008164a3469a7450f4730a0b7266def22187680
Fran Barton <francis.barton@nhs.net>
2026-04-10 18:04:19 +0100

    :heavy_plus_sign: Add config file helper function Should close #11

--- R/write_params_json.R
+++ R/write_params_json.R
@@ -48,16 +48,22 @@ create_custom_params <- function(config_file, intervals_data, ...) {
     purrr::list_modify(!!!intervals) |>
     purrr::list_modify(...)
 
+  # Currently, assigning ndg3 values is the only valid option here. But this
+  # step is set up so as to be ready to work with other options in future.
   if (params_lst[["non-demographic_adjustment"]] == "ndg3") {
     ndg3_values <- yaml12::read_yaml(get_local_sysfile("ndg3_values.yaml"))
     purrr::assign_in(params_lst, "non-demographic_adjustment", ndg3_values)
-  } else if (params_lst[["non-demographic_adjustment"]] == "ndg2") {
-    ndg2_values <- yaml12::read_yaml(get_local_sysfile("ndg2_values.yaml"))
-    purrr::assign_in(params_lst, "non-demographic_adjustment", ndg2_values)
   } else {
     params_lst
   }
 }
+
+#' Helper function to return the file path to a bundled base config YAML file
+#'
+#' @param v string The NHP version for the config file, for example "v4.4"
+#' @returns A file path to the config file, as bundled with the package
+#' @export
+get_config_filepath <- \(v) get_local_sysfile(paste0("config_", v, ".yaml"))
 
 
 get_local_sysfile <- function(...) {
